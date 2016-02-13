9b8719f1099e2c271931ec3ab519622f4d504ae0
Chel21 <Chel@activist.com>
2016-02-13 19:02:37 +0200

    Rejected commit: Default

--- classes/UFile.php
+++ classes/UFile.php
@@ -1,6 +1,5 @@
 <?php
 class UFile{
-
     function ErrorFile(){
         if ($_FILES["userfile"]["error"] != UPLOAD_ERR_OK) {
             switch ($_FILES["userfile"]["error"]) {
@@ -25,38 +24,32 @@ class UFile{
             }
         }
     }
-
     function errorSize(){
         if($_FILES["userfile"]["size"] > 153600){
             echo "Загружаемый файл больше 150кБайт";
             exit;
         }
     }
-
-
-     function errorType(){
-            if (($_FILES["userfile"]["type"] !== "image/png") and ($_FILES["userfile"]["type"] !== "image/jpeg")) {
-                echo "Не верный формат загружаемого рисунка";
+    function errorType(){
+        if (($_FILES["userfile"]["type"] !== "image/png") and ($_FILES["userfile"]["type"] !== "image/jpeg")) {
+            echo "Не верный формат загружаемого рисунка";
+            exit;
+        }
+    }
+    function errorWH(){
+        $size = (list($width, $height, $type, $attr) = getimagesize($_FILES["userfile"]["tmp_name"]));
+        switch ($size) {
+            case $width > 150:
+                echo "Ширина больше 150";
                 exit;
-            }
+            case $height > 150:
+                echo "Высота больше 150";
+                exit;
         }
-
-     function errorWH(){
-            $size = (list($width, $height, $type, $attr) = getimagesize($_FILES["userfile"]["tmp_name"]));
-                switch ($size) {
-                    case $width > 150:
-                        echo "Ширина больше 150";
-                        exit;
-                    case $height > 150:
-                        echo "Высота больше 150";
-                        exit;
-                }
-
-        }
-
-     function createPicture(){
-            echo "Размер загруженного файла: ".$_FILES["userfile"]["size"]." байт <br>";
-            echo "Тип загруженного файла: ".$_FILES["userfile"]["type"];
-            move_uploaded_file($_FILES["userfile"]["tmp_name"],"C:/img/".$_FILES["userfile"]["name"]);
-        }
+    }
+    function createPicture(){
+        echo "Размер загруженного файла: ".$_FILES["userfile"]["size"]." байт <br>";
+        echo "Тип загруженного файла: ".$_FILES["userfile"]["type"];
+        move_uploaded_file($_FILES["userfile"]["tmp_name"],"C:/img/".$_FILES["userfile"]["name"]);
+    }
 }
